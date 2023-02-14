e5452973befad8d53aea7cb5d56ca8500cf39587
Tha Leang <vleango@gmail.com>
2023-02-14 10:00:02 -0600

    wire reset password endpoints

--- datastores/graphql/users.js
+++ datastores/graphql/users.js
@@ -10,6 +10,52 @@ const getUsersQuery = gql`
   }
 `
 
+const sendPasswordResetTokenMutation = gql`
+  mutation passwordResetToken($email: String!) {
+    sendPasswordResetToken(email: $email)
+  }
+`
+
+const userResetPasswordMutation = gql`
+  mutation userResetPassword(
+    $resetPasswordToken: String!
+    $password: String!
+    $passwordConfirmation: String!
+  ) {
+    userResetPassword(
+      resetPasswordToken: $resetPasswordToken
+      password: $password
+      passwordConfirmation: $passwordConfirmation
+    )
+  }
+`
+
+export async function sendPasswordResetToken({ email }) {
+  const results = await client().mutate({
+    variables: {
+      email,
+    },
+    mutation: sendPasswordResetTokenMutation,
+  })
+  return results.data.sendPasswordResetToken
+}
+
+export async function userResetPassword({
+  resetPasswordToken,
+  password,
+  passwordConfirmation,
+}) {
+  const results = await client().mutate({
+    variables: {
+      resetPasswordToken,
+      password,
+      passwordConfirmation,
+    },
+    mutation: userResetPasswordMutation,
+  })
+  return results.data.userResetPassword
+}
+
 export async function getServerUsers({ session }) {
   const results = await client({ session }).query({
     variables: {},
--- pages/forgot.js
+++ pages/forgot.js
@@ -1,8 +1,18 @@
+import { useRef } from "react"
 import { useRouter } from "next/router"
 import { toastSuccess } from "components/notification"
+import { sendPasswordResetToken } from "datastores/graphql/users"
 
 export default function ForgotPasswordPage() {
   const router = useRouter()
+  const inputEmailRef = useRef()
+
+  function submitBtnClicked(e) {
+    e.preventDefault()
+    sendPasswordResetToken({ email: inputEmailRef.current.value })
+    toastSuccess("Please check your email for password reset link!")
+    router.push(`/login`)
+  }
 
   return (
     <form className="mx-auto pt-12 space-y-8 w-3/6">
@@ -22,7 +32,7 @@ export default function ForgotPasswordPage() {
           <div className="grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
             <div className="sm:col-span-4">
               <label
-                for="email"
+                htmlFor="email"
                 className="block text-sm font-medium text-gray-700"
               >
                 Email address
@@ -32,7 +42,8 @@ export default function ForgotPasswordPage() {
                   id="email"
                   name="email"
                   type="email"
-                  autocomplete="email"
+                  autoComplete="email"
+                  ref={inputEmailRef}
                   className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                 />
               </div>
@@ -52,11 +63,7 @@ export default function ForgotPasswordPage() {
           <button
             type="submit"
             className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            onClick={(e) => {
-              e.preventDefault()
-              toastSuccess("Please check your email for password reset link!")
-              router.push(`/login`)
-            }}
+            onClick={submitBtnClicked}
           >
             Submit
           </button>
